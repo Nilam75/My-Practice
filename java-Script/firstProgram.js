a68b23a027e47cdcbbb8e666cf695c0565d22912
Nilam75 <kalenilam@gmail.com>
2023-07-28 13:03:04 +0530

    first program of Js

---
+++ java-Script/firstProgram.js
@@ -0,0 +1,10 @@
+let hellofriends=document.querySelector('button');
+hellofriends.addEventListener('click', showMe);
+function showMe(){
+    alert("Welcome ");
+}
+hellofriends.addEventListener('click', inputeName);
+function inputeName(){
+    let name =prompt('Enter the student name');
+    hellofriends.textContent=('Roll No.1'+name);
+} 
