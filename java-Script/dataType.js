ec983a9c47ec23357c7043f5883a03ed537b7631
Nilam75 <kalenilam@gmail.com>
2023-07-31 11:00:50 +0530

    All data type and practics

---
+++ java-Script/dataType.js
@@ -0,0 +1,31 @@
+// data type:1string,number,bigint,boolean,undifined,null,symbol,object there are type of data type.
+// that all data type are premative data type:mean we want to store single value that time used premative data type.
+// STRING DATA TYPE('',"")
+var pName='nilam'
+console.log(pName);
+console.log(typeof(pName));
+
+// NUMBER DATA TYPE(NUMBER OR INTEGER )
+let cash=5482
+console.log(cash);
+console.log(typeof(cash));
+// BOOLEAN DATA TYPE:(There value true or fales:when we want to pass value of true or fals used boolean)
+let pass=false
+console.log(pass);
+console.log(typeof(pass));
+// Null data type:the null data type is object.it shows the nothing
+let mark=null
+console.log(mark);
+console.log(typeof(mark));
+// Object:this data type used to multipal value you want to passed it used to {}
+let student={
+    studentName:'nill',
+    age:15,
+    contact:1425426
+}
+console.log(student);
+console.log(typeof(student));
+// Array :dispay the list of itemin [] brakets.
+let list=['nilam','vaibhav','neev']
+console.log(list);
+console.log(typeof(list));
