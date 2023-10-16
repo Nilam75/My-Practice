787997d5975d692e3adc47382b046c0cdfd74bd7
Nilam75 <kalenilam@gmail.com>
2023-10-16 14:46:07 +0530

    bootstrap ins and form validation

--- new_Angular_project/src/app/principle/principle-sign-up/principle-sign-up.component.ts
+++ new_Angular_project/src/app/principle/principle-sign-up/principle-sign-up.component.ts
@@ -8,7 +8,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PrincipleSignUpComponent {
   SinUpForm!:FormGroup;
-
+  age = 20;
+  // todayDate = new Date();
+  todayDate=new Date();
+  userAge: any;
+  datePattern = /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/;
+  isGenderSelected = false;
+showPass=false;
   constructor(public fb:FormBuilder){}
 
   ngOnInit(){
@@ -17,15 +23,42 @@ export class PrincipleSignUpComponent {
 
   formDetails(){
     this.SinUpForm=this.fb.group({
-      userName:['',[Validators.maxLength(10)]],
+      userName:['',[Validators.maxLength(10),Validators.minLength(5),Validators.pattern('[a-zA-Z ]+')]],
       // ['poonam',[Validators.maxLength(10)]],
-      emailId:[]
+      mobNo:['',[Validators.pattern('[0-9+]*')]],
+      emailId:['',[Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      hobies:['',[Validators.requiredTrue]],
+      password:[false,Validators.required],
+      gender:[''],
+      dob:['',Validators.pattern(this.datePattern)]
     })
   }
 
   button(){
-    console.log('form Data',this.SinUpForm.value);
+    let gender=this.SinUpForm.value.gender;
+    if(gender){
+      console.log('form Data',this.SinUpForm.value);
+    }
+    else{
+      this.isGenderSelected=true
+            return
+    }
     
+  }
+  gender(){
+    this.isGenderSelected=false;
+  }
+
+  calYear(){
+    
+    let dobFieldValue = this.SinUpForm.value.dob;
+    let todayFullYear = this.todayDate.getFullYear();
+    let splitedDate = dobFieldValue?.split('/');
+    let usersFullYear = splitedDate[2];
+    this.userAge = todayFullYear - usersFullYear;
+  }
+  showPassword(){
+    this.showPass=!this.showPass;
   }
 }
 
