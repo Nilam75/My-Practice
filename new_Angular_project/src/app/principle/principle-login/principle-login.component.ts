99fc7441ce6c2aae007c036e8e6c237de604b06b
Nilam75 <kalenilam@gmail.com>
2023-10-21 16:25:59 +0530

    templet driven form and forgate password form

--- new_Angular_project/src/app/principle/principle-login/principle-login.component.ts
+++ new_Angular_project/src/app/principle/principle-login/principle-login.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./principle-login.component.scss']
 })
 export class PrincipleLoginComponent {
+userName:any;
+formHide=true;
+newwpassword:any;
+confirmPassword:any;
+isMatch:boolean=false;
 
+
+  submit(form: any) {
+    console.log(form);
+
+  }
+
+  forgatPasswod(){
+    console.log('forgatPassword');
+    
+    this.formHide=false
+  }
+
+  newPasswordfild(passdata:any){
+    this.newwpassword=passdata;
+  }
+
+  confirmPasFild(confirmPassword:any){
+    this.confirmPassword=confirmPassword;
+    if(this.newwpassword==this.confirmPassword){
+      this.isMatch=false;
+    }else{
+      this.isMatch=true;
+    }
+  }
 }
