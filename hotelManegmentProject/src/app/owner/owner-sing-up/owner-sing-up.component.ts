111cf892488af5f560e3b2a944ccbbae622c976b
Nilam75 <kalenilam@gmail.com>
2023-11-24 16:06:05 +0530

    Owner Sing Up form With Validation

--- hotelManegmentProject/src/app/owner/owner-sing-up/owner-sing-up.component.ts
+++ hotelManegmentProject/src/app/owner/owner-sing-up/owner-sing-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-owner-sing-up',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./owner-sing-up.component.scss']
 })
 export class OwnerSingUpComponent {
+  sinupForm!:FormGroup;
+  showWorning:boolean=true;
+  gender:boolean=false;
+  showpass=false;
+constructor(private fb:FormBuilder,private errorMath:ErrorStateMatcher){}
+ngOnInit(){
+  this.formDetails()
+}
 
+formDetails(){
+  this.sinupForm=this.fb.group({
+    firstName:['',Validators.required],
+    midName:['',Validators.required],
+    lastName:['',Validators.required],
+    email:['',Validators.required,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)],
+    mobileNumber: ['', [Validators.required, Validators.pattern('[0-9+]*')]],
+    gender:['',Validators.required],
+    userName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]*$')]],
+    pass: ['', [Validators.required, Validators.minLength(8)]]
+  })
+}
+
+hideHint(){
+  this.showWorning=!this.showWorning;
+}
+
+isShowPassword(){
+  this.showpass=!this.showpass
+}
+
+  submitSingUpForm(){
+   
+    let gen=this.sinupForm.value.gender
+    if(gen){
+      console.log(this.sinupForm.value);
+    }else{
+      this.gender=true;
+      return 
+    }
+   
+  }
 }
--- hotelManegmentProject/src/app/share/share.module.ts
+++ hotelManegmentProject/src/app/share/share.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
 @NgModule({
   declarations: [],
   imports: [
@@ -11,13 +12,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatButtonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatRadioModule
   ],
   exports:[
     MatInputModule,
     MatButtonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule, 
+    MatFormFieldModule,
+    MatRadioModule
   ]
 })
 export class ShareModule { }
